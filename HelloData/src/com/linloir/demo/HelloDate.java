e6656424480c54062566bb928caf06ec812b1934
JonathanZhang020706 <3145078758@qq.com>
2020-10-11 18:02:46 +0800

    Test upload
    none

---
+++ HelloData/src/com/linloir/demo/HelloDate.java
@@ -0,0 +1,14 @@
+//HelloDate.java
+/*
+    Date:2020-10-11
+    Author:Linloir
+ */
+package com.linloir.demo;
+import java.util.*;
+
+public class HelloDate {
+    public static void main(String[] args) {
+        System.out.println("Hello, it's: ");
+        System.out.println(new Date());
+    }
+}

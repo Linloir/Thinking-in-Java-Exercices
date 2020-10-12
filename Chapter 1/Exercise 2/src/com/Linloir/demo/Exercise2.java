412a1585b2c0fe3da15215c223c7b1c264ac3b64
JonathanZhang020706 <3145078758@qq.com>
2020-10-12 19:43:08 +0800

    new Exercise
    none

---
+++ Chapter 1/Exercise 2/src/com/Linloir/demo/Exercise2.java
@@ -0,0 +1,19 @@
+//:Create a Hello World application
+/**
+ * Exercise 2 of Chapter 2
+ * @author Linloir
+ * @author Jonathan Zhang
+ * @version 1.0
+ * @since 2020-10-12
+ */
+
+package com.Linloir.demo;
+import java.util.*;         //import although it'n not necessary
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}/*Output:
+Hello World
+*/

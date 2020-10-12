e38cd68a96d18e40d991abbbf36a8b7d377a2b54
JonathanZhang020706 <3145078758@qq.com>
2020-10-12 21:07:49 +0800

    New Exercise
    Linloir

---
+++ Chapter 2/Exercise 5/src/com/Linloir/demo/Exercise5.java
@@ -0,0 +1,31 @@
+//:Revise the previous exercise, evaluate the data and print them
+/**
+ * Exercise 5 of Chapter 2
+ * @author Linloir
+ * @author Jonathan Zhang
+ * @version 1.0
+ * @since 2020-10-12
+ */
+
+package com.Linloir.demo;
+import java.util.*;
+
+public class Exercise5 {
+    int i;
+    double d;
+    boolean b;
+    public static void main(String[] args) {
+        Exercise5 d = new Exercise5();
+        d.i = 47;
+        d.d = 1.1;
+        d.b = false;
+        System.out.println("i = " + d.i);
+        System.out.println("d = " + d.d);
+        System.out.println("b = " + d.b);
+    }
+}/*Output:
+i = 47
+d = 1.1
+b = false
+*/
+

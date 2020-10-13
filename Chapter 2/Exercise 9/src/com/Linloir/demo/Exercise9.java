b61f7a3582f48dc253071f815cc07a3c37361c5b
JonathanZhang020706 <3145078758@qq.com>
2020-10-13 14:21:09 +0800

    New Exercise
    Linloir

---
+++ Chapter 2/Exercise 9/src/com/Linloir/demo/Exercise9.java
@@ -0,0 +1,19 @@
+//:Code a program demonstrating auto packaging function works for all primitive type and wrapper type
+/**
+ * Exercise 9 of Chapter 2
+ * @author Linloir
+ * @author Jonathan Zhang
+ * @version 1.0 2020-10-13
+ */
+
+package com.Linloir.demo;
+import java.util.*;
+
+public class Exercise9 {
+    public static void main(String[] args) {
+        Character ch = 'x';
+        System.out.println(ch);
+    }
+}/*Output:
+x
+*/

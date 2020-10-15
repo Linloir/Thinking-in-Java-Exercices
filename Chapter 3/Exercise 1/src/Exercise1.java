88067fa76d72add6f2c125abde35c55de3da5445
JonathanZhang020706 <3145078758@qq.com>
2020-10-15 17:31:07 +0800

    New Exercise
    Linloir

---
+++ Chapter 3/Exercise 1/src/Exercise1.java
@@ -0,0 +1,20 @@
+//:using library net.mindview.Print.* to make more concise(简洁的) program
+/**
+ * Exercise 1 of Chapter 3
+ * @author Linloir
+ * @author Jonathan Zhang
+ * @version 1.0 2020-10-15
+ */
+
+import java.util.*;
+import static net.mindview.Print.*;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        print("Hello World");
+        System.out.println("Hello World!");
+    }
+}/*Output:
+Hello World
+Hello World!
+*/
